ebb64c73d196fb833877af7ae6597514de12bd67
JackCarrkeally <Jackck21@gmail.com>
2024-03-21 10:20:48 +0000

    New version with backend connection and sql

---
+++ BlazorTest/Program.cs
@@ -0,0 +1,26 @@
+using XLNSupportBot;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+// Configure HttpClient to use the same base address as the server
+
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
+
+
+
+
